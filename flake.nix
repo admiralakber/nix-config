19b81740a74062f5d42d583692bad954cba353be
Aqeel Akber <aqeel@aqeelakber.com>
2024-11-21 20:12:00 +1100

    fix: backup extension convention

--- flake.nix
+++ flake.nix
@@ -29,7 +29,7 @@
           home-manager.nixosModules.home-manager {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.backupFileExtension = "backup";
+            home-manager.backupFileExtension = "bak";
 
             home-manager.extraSpecialArgs = inputs // specialArgs;
             home-manager.users.${username} = import ./users/${username}/home.nix;
