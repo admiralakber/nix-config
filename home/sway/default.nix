49d25d7cf0e0c3ff0d8b923ec587c1a1235f684b
Aqeel Akber <aqeel@aqeelakber.com>
2024-12-28 20:51:19 +1100

    feat: removed picom and theming

--- home/sway/default.nix
+++ home/sway/default.nix
@@ -1,10 +1,5 @@
 { config, pkgs, ... }: {
 
-  home.packages = with pkgs; [
-    picom
-  ];
-
   home.file.".config/sway/wallpaper.jpg".source = ./wallpaper.jpg;
   home.file.".config/sway/config".source = ./config;
-  home.file.".config/picom/picom.conf".source = ./picom.conf;
 }
