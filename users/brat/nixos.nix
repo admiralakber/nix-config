a5708ca6961ce095e9ef41a174e5a009d15b2c5c
Aqeel Akber <aqeel@aqeelakber.com>
2025-04-17 09:47:22 +1000

    fix(oci): 🐛 container workflow fixes.

--- users/brat/nixos.nix
+++ users/brat/nixos.nix
@@ -2,6 +2,20 @@
 
 {
   users.users.brat = {
+    isNormalUser = true;
+    home = "/home/brat";
+    group = "users";
+    extraGroups = [
+      "wheel"
+      "podman"
+      "libvirtd"
+    ];
+    subUidRanges = [
+      { startUid = 100000; count = 65536; }
+    ];
+    subGidRanges = [
+      { startGid = 100000; count = 65536; }
+    ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB+/HfuySXO9jga3DhI+Wc1Te+SGuUwUa4TvykT1VsTS aqeel@empathy"
     ];
